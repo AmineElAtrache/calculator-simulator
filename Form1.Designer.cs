456de84dd4ffb571300e9dfcb2a0e61eac4b3237
AmineElAtrache <amine.07.atrache@gmail.com>
2025-01-28 00:11:00 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿
+namespace TpCalculette
+{
+    partial class frmCalculette
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCalculette));
+            this.buttonEqual = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.buttonPoint = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonLess = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.buttonDivision = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.buttonM = new System.Windows.Forms.Button();
+            this.buttonNegate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonDeleteAll = new System.Windows.Forms.Button();
+            this.textBoxAffichage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonEqual
+            // 
+            resources.ApplyResources(this.buttonEqual, "buttonEqual");
+            this.buttonEqual.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.buttonEqual.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonEqual.Name = "buttonEqual";
+            this.buttonEqual.UseVisualStyleBackColor = false;
+            this.buttonEqual.Click += new System.EventHandler(this.buttonEqual_Click);
+            // 
+            // button0
+            // 
+            resources.ApplyResources(this.button0, "button0");
+            this.button0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.button0.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button0.Name = "button0";
+            this.button0.UseVisualStyleBackColor = false;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // buttonPoint
+            // 
+            resources.ApplyResources(this.buttonPoint, "buttonPoint");
+            this.buttonPoint.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.buttonPoint.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonPoint.Name = "buttonPoint";
+            this.buttonPoint.UseVisualStyleBackColor = false;
+            this.buttonPoint.Click += new System.EventHandler(this.buttonPoint_Click);
+            // 
+            // buttonPlus
+            // 
+            resources.ApplyResources(this.buttonPlus, "buttonPlus");
+            this.buttonPlus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.buttonPlus.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.UseVisualStyleBackColor = false;
+            this.buttonPlus.Click += new System.EventHandler(this.buttonPlus_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonLess
+            // 
+            resources.ApplyResources(this.buttonLess, "buttonLess");
+            this.buttonLess.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.buttonLess.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonLess.Name = "buttonLess";
+            this.buttonLess.UseVisualStyleBackColor = false;
+            this.buttonLess.Click += new System.EventHandler(this.buttonLess_Click);
+            // 
+            // button6
+            // 
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.button6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // buttonDivision
+            // 
+            resources.ApplyResources(this.buttonDivision, "buttonDivision");
+            this.buttonDivision.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.buttonDivision.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonDivision.Name = "buttonDivision";
+            this.buttonDivision.UseVisualStyleBackColor = false;
+            this.buttonDivision.Click += new System.EventHandler(this.buttonDivision_Click);
+            // 
+            // button9
+            // 
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.button9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button9.Name = "button9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.button8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button26_Click);
+            // 
+            // button7
+            // 
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.button7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button25_Click);
+            // 
+            // buttonM
+            // 
+            resources.ApplyResources(this.buttonM, "buttonM");
+            this.buttonM.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.buttonM.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.buttonM.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonM.Name = "buttonM";
+            this.buttonM.UseVisualStyleBackColor = false;
+            this.buttonM.Click += new System.EventHandler(this.buttonM_Click);
+            // 
+            // buttonNegate
+            // 
+            resources.ApplyResources(this.buttonNegate, "buttonNegate");
+            this.buttonNegate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.buttonNegate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonNegate.Name = "buttonNegate";
+            this.buttonNegate.UseVisualStyleBackColor = false;
+            this.buttonNegate.Click += new System.EventHandler(this.buttonNegate_Click);
+            // 
+            // buttonDelete
+            // 
+            resources.ApplyResources(this.buttonDelete, "buttonDelete");
+            this.buttonDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.buttonDelete.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonDeleteAll
+            // 
+            resources.ApplyResources(this.buttonDeleteAll, "buttonDeleteAll");
+            this.buttonDeleteAll.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(59)))), ((int)(((byte)(72)))));
+            this.buttonDeleteAll.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonDeleteAll.Name = "buttonDeleteAll";
+            this.buttonDeleteAll.UseVisualStyleBackColor = false;
+            this.buttonDeleteAll.Click += new System.EventHandler(this.buttonDeleteAll_Click);
+            // 
+            // textBoxAffichage
+            // 
+            resources.ApplyResources(this.textBoxAffichage, "textBoxAffichage");
+            this.textBoxAffichage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(43)))));
+            this.textBoxAffichage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxAffichage.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBoxAffichage.Name = "textBoxAffichage";
+            this.textBoxAffichage.ReadOnly = true;
+            this.textBoxAffichage.TextChanged += new System.EventHandler(this.textBoxAffichage_TextChanged);
+            // 
+            // frmCalculette
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(32)))), ((int)(((byte)(43)))));
+            this.Controls.Add(this.buttonEqual);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.buttonPoint);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonLess);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.buttonDivision);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.buttonM);
+            this.Controls.Add(this.buttonNegate);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonDeleteAll);
+            this.Controls.Add(this.textBoxAffichage);
+            this.Name = "frmCalculette";
+            this.Load += new System.EventHandler(this.frmCalculette_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEqual;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button buttonPoint;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonLess;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonDivision;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button buttonM;
+        private System.Windows.Forms.Button buttonNegate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonDeleteAll;
+        private System.Windows.Forms.TextBox textBoxAffichage;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,320 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    namespace TpCalculette
+    {
+        public partial class frmCalculette : Form
+        {
+            public frmCalculette()
+            {
+                InitializeComponent();
+
+            }
+
+
+            double result;
+            double[] numbers = new double[2];
+            double currentNumber;
+            string operatorSymbol;
+            bool isNewCalculation = true;
+            private void button25_Click(object sender, EventArgs e)//test
+            {
+                NumberButtonClick(sender, e);
+
+            }
+
+            private void button26_Click(object sender, EventArgs e)
+            {
+                NumberButtonClick(sender, e);
+
+
+            }
+
+
+            private void button9_Click(object sender, EventArgs e)
+            {
+                NumberButtonClick(sender, e);
+
+            }
+
+            private void button4_Click(object sender, EventArgs e)
+            {
+                NumberButtonClick(sender, e);
+
+            }
+
+            private void button5_Click(object sender, EventArgs e)
+            {
+                NumberButtonClick(sender, e);
+
+
+            }
+
+            private void button6_Click(object sender, EventArgs e)
+            {
+                NumberButtonClick(sender, e);
+
+            }
+
+            private void button3_Click(object sender, EventArgs e)
+            {
+                NumberButtonClick(sender, e);
+
+
+            }
+
+            private void button2_Click(object sender, EventArgs e)
+            {
+                NumberButtonClick(sender, e);
+
+
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                NumberButtonClick(sender, e);
+
+            }
+            private void button0_Click(object sender, EventArgs e)
+            {
+                NumberButtonClick(sender, e);
+
+            }
+
+
+            private void buttonNegate_Click(object sender, EventArgs e)
+            {
+                double currentValue = double.Parse(textBoxAffichage.Text);
+                currentValue = -currentValue;
+                textBoxAffichage.Text = currentValue.ToString();
+                currentNumber = currentValue;
+
+                if (!isNewCalculation)
+                {
+                    numbers[0] = currentValue;
+                }
+            }
+
+            private void buttonM_Click(object sender, EventArgs e)
+            {
+                PerformOperation();
+                operatorSymbol = "*";
+            }
+
+            private double runningTotal = 0.0;
+
+
+            private void textBoxAffichage_TextChanged(object sender, EventArgs e)
+            {
+
+            }
+
+            private void NumberButtonClick(object sender, EventArgs e)
+            {
+                Button button = (Button)sender;
+
+                if (isNewCalculation)
+                {
+                    textBoxAffichage.Text = button.Text;
+                    isNewCalculation = false;
+                }
+                else
+                {
+                    textBoxAffichage.Text += button.Text;
+                }
+
+                currentNumber = double.Parse(textBoxAffichage.Text);
+            }
+
+            private void buttonPoint_Click(object sender, EventArgs e)
+            {
+                if (!textBoxAffichage.Text.Contains("."))
+                {
+                    textBoxAffichage.Text += ".";
+                }
+            }
+
+
+            private void buttonPlus_Click(object sender, EventArgs e)
+            {
+                PerformOperation();
+                operatorSymbol = "+";
+            }
+
+            private void buttonLess_Click(object sender, EventArgs e)
+            {
+                PerformOperation();
+                operatorSymbol = "-";
+            }
+
+            private void buttonDivision_Click(object sender, EventArgs e)
+            {
+                PerformOperation();
+                operatorSymbol = "/";
+            }
+            private void PerformOperation()
+            {
+                if (!isNewCalculation)
+                {
+                    numbers[1] = currentNumber;
+
+                    if (numbers.Length >= 2)
+                    {
+                        switch (operatorSymbol)
+                        {
+                            case "+":
+                                currentNumber = numbers[0] + numbers[1];
+                                break;
+                            case "-":
+                                currentNumber = numbers[0] - numbers[1];
+                                break;
+                            case "*":
+                                currentNumber = numbers[0] * numbers[1];
+                                break;
+                            case "/":
+                                if (numbers[1] != 0)
+                                    currentNumber = numbers[0] / numbers[1];
+                                else if (numbers[1] == 0)
+                                {
+                                    MessageBox.Show("Cannot divide by zero.");
+                                }
+
+
+                                break;
+                        }
+
+                        numbers[0] = currentNumber;
+
+                        textBoxAffichage.Text = currentNumber.ToString();
+                        isNewCalculation = true;
+                    }
+                }
+                else
+                {
+                    // Reset the array
+                    numbers = new double[2];
+                    numbers[0] = currentNumber;
+                }
+            }
+
+
+            private void buttonEqual_Click(object sender, EventArgs e)
+            {
+                PerformOperation();
+                isNewCalculation = true;
+            }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (!isNewCalculation)
+            {
+                if (textBoxAffichage.Text.Length > 1)
+                {
+                    textBoxAffichage.Text = textBoxAffichage.Text.Substring(0, textBoxAffichage.Text.Length - 1);
+                    currentNumber = double.Parse(textBoxAffichage.Text);
+                }
+                else
+                {
+                    textBoxAffichage.Text = "0";
+                    currentNumber = 0;
+                    isNewCalculation = true;
+                }
+            }
+        }
+
+
+        private void buttonDeleteAll_Click(object sender, EventArgs e)
+            {
+                numbers = new double[2];
+                currentNumber = 0;
+                operatorSymbol = "";
+                isNewCalculation = true;
+                textBoxAffichage.Text = "0";
+            }
+
+
+
+            private void frmCalculette_KeyPress(object sender, KeyPressEventArgs e)
+            {
+                // Handle numeric keys
+                if (char.IsDigit(e.KeyChar))
+                {
+                    NumberKeyPress(e.KeyChar);
+                }
+                else if (e.KeyChar == '.')
+                {
+                    buttonPoint_Click(sender, e);
+                }
+                else if (e.KeyChar == (char)Keys.Enter)
+                {
+                    buttonEqual_Click(sender, e);
+                }
+                else if (e.KeyChar == (char)Keys.Back)
+                {
+                    buttonDelete_Click(sender, e);
+                }
+                else if (e.KeyChar == '+')
+                {
+                    buttonPlus_Click(sender, e);
+                }
+                else if (e.KeyChar == '-')
+                {
+                    buttonLess_Click(sender, e);
+                }
+                else if (e.KeyChar == '*')
+                {
+                    buttonM_Click(sender, e);
+                }
+                else if (e.KeyChar == '/')
+                {
+                    buttonDivision_Click(sender, e);
+                }
+            }
+
+            private void NumberKeyPress(char key)
+            {
+                if (isNewCalculation)
+                {
+                    textBoxAffichage.Text = key.ToString();
+                    isNewCalculation = false;
+                }
+                else
+                {
+                    textBoxAffichage.Text += key;
+                }
+
+                currentNumber = double.Parse(textBoxAffichage.Text);
+            }
+
+            private void frmCalculette_Load(object sender, EventArgs e)
+            {
+                // Subscribe to the KeyPress event when the form loads
+                this.KeyPress += frmCalculette_KeyPress;
+                // Set KeyPreview property of the form to true to capture key events
+                this.KeyPreview = true;
+            }
+
+            private void textBox1_TextChanged(object sender, EventArgs e)
+            {
+
+            }
+
+            private void textBox1_TextChanged_1(object sender, EventArgs e)
+            {
+
+            }
+
+
+        private void RERDED_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+    }
